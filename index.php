9adacee4a16373a39237486f44e1c0a103eb7e70
amaynut <said.guerrab@gmail.com>
2014-10-10 09:36:35 -0400

    footer and navbar second version

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+ // TODO Fill the menu
+ // TODO play with color
+ // TODO make it online with private access
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,10 +11,13 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="description" content="Dynagroup Technologies, specialiste des solutions de dépoussièrage sur mesure.">
+    <meta name="author" content="Dynagroup Technologies">
+    <!-- Favicon image -->
+    <link rel="shortcut icon" href="./images/favicon.ico" />
+    <link rel="apple-touch-icon" href="./images/favicon.ico" />
 
-    <title>Modern Business - Start Bootstrap Template</title>
+    <title>Dynagroup Technologies - Page d'accueil</title>
 
     <!-- Bootstrap Core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
@@ -32,131 +40,15 @@
 
 <body>
 
-    <!-- Navigation -->
-    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="index.php">Dynagroup Technologies</a>
-            </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav navbar-right">
-                    <li>
-                        <a href="about.html">About</a>
-                    </li>
-                    <li>
-                        <a href="services.html">Services</a>
-                    </li>
-                    <li>
-                        <a href="contact.html">Contact</a>
-                    </li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Portfolio <b class="caret"></b></a>
-                        <ul class="dropdown-menu">
-                            <li>
-                                <a href="portfolio-1-col.html">1 Column Portfolio</a>
-                            </li>
-                            <li>
-                                <a href="portfolio-2-col.html">2 Column Portfolio</a>
-                            </li>
-                            <li>
-                                <a href="portfolio-3-col.html">3 Column Portfolio</a>
-                            </li>
-                            <li>
-                                <a href="portfolio-4-col.html">4 Column Portfolio</a>
-                            </li>
-                            <li>
-                                <a href="portfolio-item.html">Single Portfolio Item</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Blog <b class="caret"></b></a>
-                        <ul class="dropdown-menu">
-                            <li>
-                                <a href="blog-home-1.html">Blog Home 1</a>
-                            </li>
-                            <li>
-                                <a href="blog-home-2.html">Blog Home 2</a>
-                            </li>
-                            <li>
-                                <a href="blog-post.html">Blog Post</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Other Pages <b class="caret"></b></a>
-                        <ul class="dropdown-menu">
-                            <li>
-                                <a href="full-width.html">Full Width Page</a>
-                            </li>
-                            <li>
-                                <a href="sidebar.html">Sidebar Page</a>
-                            </li>
-                            <li>
-                                <a href="faq.html">FAQ</a>
-                            </li>
-                            <li>
-                                <a href="404.html">404</a>
-                            </li>
-                            <li>
-                                <a href="pricing.html">Pricing Table</a>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-    </nav>
 
-    <!-- Header Carousel -->
-    <header id="myCarousel" class="carousel slide">
-        <!-- Indicators -->
-        <ol class="carousel-indicators">
-            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-            <li data-target="#myCarousel" data-slide-to="1"></li>
-            <li data-target="#myCarousel" data-slide-to="2"></li>
-        </ol>
+    <?php
+    //Navigation
+    include_once("./partials/menu.html");
 
-        <!-- Wrapper for slides -->
-        <div class="carousel-inner">
-            <div class="item active">
-                <div class="fill" style="background-image:url('./images/iStock_Medium.jpg');"></div>
-                <div class="carousel-caption">
-                    <h2>Caption 1</h2>
-                </div>
-            </div>
-            <div class="item">
-                <div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide Two');"></div>
-                <div class="carousel-caption">
-                    <h2>Caption 2</h2>
-                </div>
-            </div>
-            <div class="item">
-                <div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide Three');"></div>
-                <div class="carousel-caption">
-                    <h2>Caption 3</h2>
-                </div>
-            </div>
-        </div>
+    // header carousel
+    include_once("./partials/carousel.html");
+    ?>
 
-        <!-- Controls -->
-        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-            <span class="icon-prev"></span>
-        </a>
-        <a class="right carousel-control" href="#myCarousel" data-slide="next">
-            <span class="icon-next"></span>
-        </a>
-    </header>
 
     <!-- Page Content -->
     <div class="container">
@@ -165,7 +57,7 @@
         <div class="row">
             <div class="col-lg-12">
                 <h1 class="page-header">
-                   Bienvenue chez Dynagroup technologies
+                   Bienvenue chez Dynagroup technologies. <small>Solutions de dépoussièrage sur mesure</small>
                 </h1>
             </div>
             <div class="col-md-4">
@@ -175,7 +67,7 @@
                     </div>
                     <div class="panel-body">
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, optio corporis quae nulla aspernatur in alias at numquam rerum ea excepturi expedita tenetur assumenda voluptatibus eveniet incidunt dicta nostrum quod?</p>
-                        <a href="#" class="btn btn-default">Learn More</a>
+                        <a href="#" class="btn btn-default">Lire la suite</a>
                     </div>
                 </div>
             </div>
@@ -186,7 +78,7 @@
                     </div>
                     <div class="panel-body">
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, optio corporis quae nulla aspernatur in alias at numquam rerum ea excepturi expedita tenetur assumenda voluptatibus eveniet incidunt dicta nostrum quod?</p>
-                        <a href="#" class="btn btn-default">Learn More</a>
+                        <a href="#" class="btn btn-default">Lire la suite</a>
                     </div>
                 </div>
             </div>
@@ -197,7 +89,7 @@
                     </div>
                     <div class="panel-body">
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, optio corporis quae nulla aspernatur in alias at numquam rerum ea excepturi expedita tenetur assumenda voluptatibus eveniet incidunt dicta nostrum quod?</p>
-                        <a href="#" class="btn btn-default">Learn More</a>
+                        <a href="#" class="btn btn-default">Lire la suite</a>
                     </div>
                 </div>
             </div>
@@ -289,8 +181,10 @@
 
     </div>
     <!-- /.container -->
+
+    <!-- footer-->
     <?php
-    include_once("./partials/footer.html");
+    include_once("./partials/footer.php");
     ?>
 
     <!-- jQuery Version 1.11.0 -->
---
+++ partials/footer.php
@@ -0,0 +1,61 @@
+<div id="footer">
+    <div class="container">
+        <div class="row">
+            <br/>
+            <div class="col-md-4">
+                <!--img src="http://oi60.tinypic.com/w8lycl.jpg" class="img-circle" alt="the-brains"-->
+                   <span class="fa-stack fa-3x">
+                    <i class="fa fa-circle fa-stack-2x text-primary"></i>
+                    <i class="fa  fa-headphones fa-stack-1x fa-inverse"></i>
+                </span>
+                <br>
+                <h4 class="footertext">Nous contacter</h4>
+                <ul class="footertext list-unstyled">
+                    <li><i class="fa fa-phone"></i> Téléphone: 514.337.3377</li>
+                    <li><i class="fa fa-fax"></i> Fax: 514-832-0989</li>
+                    <li><i class="fa fa-envelope-o"></i> Courriel: <a href="">dynagroup@dynagroup-tech.com</a></li>
+                </ul>
+            </div>
+            <div class="col-md-4">
+                <!--img src="http://oi60.tinypic.com/2z7enpc.jpg" class="img-circle" alt="..."-->
+                <span class="fa-stack fa-3x">
+                    <i class="fa fa-circle fa-stack-2x text-primary"></i>
+                    <i class="fa  fa-map-marker fa-stack-1x fa-inverse"></i>
+                </span>
+                <br>
+                <h4 class="footertext">Nous rendre visite</h4>
+                <p class="footertext"><i class="fa fa-clock-o"></i>
+                    <abbr title="Hours d'affaires">H</abbr>: Lundi - Vendredi: 8:00  à 17:00 PM</p>
+                <address>
+                    <ul class="footertext list-unstyled" >
+                        <li>
+                            Dynagroup Technologies
+                        </li>
+                        <li> 4070 Boulevard Poirier,</li>
+                        <li> Ville St-Laurent, Québec,</li>
+                        <li>Canada, H4R 2A5</li>
+                    </ul>
+                </address>
+
+            </div>
+            <div class="col-md-4">
+              <!--img src="http://oi61.tinypic.com/307n6ux.jpg" class="img-circle" alt="..."-->
+                <span class="fa-stack fa-3x">
+                    <i class="fa fa-circle fa-stack-2x text-primary"></i>
+                    <i class="fa  fa-thumbs-o-up fa-stack-1x fa-inverse"></i>
+                </span>
+                <br>
+                <h4 class="footertext">Retrouvez-nous sur le web</h4>
+                <?php
+                include("/partials/socialbar.html");
+                ?>
+            </div>
+        </div>
+        <div class="row">
+            <div class="span12">
+                <p class="footertext text-center">Dynagroup Technologies Copyright &copy; 2014</p>
+            </div>
+
+        </div>
+    </div>
+</div>
