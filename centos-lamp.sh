7db97bd7120a1f8227bf18ec8fd874b8b7829999
amaynut <said.guerrab@gmail.com>
2015-05-06 08:40:58 -0400

    Update centos-lamp.sh

--- centos-lamp.sh
+++ centos-lamp.sh
@@ -29,8 +29,29 @@ service mysqld start
 
 mysql -u root -e "SHOW DATABASES";
 
+# install some php extensions
+# mcrypt
+#sudo wget http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
+#sudo rpm -ivh epel-release-7-5.noarch.rpm
+#sudo yum install -y --enablerepo="epel" php-mcrypt
+# mbstring
+sudo yum -y install php-mbstring
+sudo echo "extension=php_mbstring.so" >> /etc/php.ini
+# mcrypt
+sudo yum -y install php-mcrypt
+# ext-dom
+sudo yum -y install php-xml
+
 # Download Started Content from github
 cd /vagrant
 sudo -u vagrant git clone https://github.com/laravel/laravel.git
+
+# install composer
+sudo curl -sS https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer
+
+# run composer install in Laravel project
+cd /vagrant/laravel
+composer install
 
 service httpd restart
